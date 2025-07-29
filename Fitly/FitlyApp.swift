9800ffba6de695428c2b75e76dc164cbaf676bfe
Nandika Peddapuram <nandikarp10@gmail.com>
2025-07-29 12:58:14 -0500

    Initial Commit

---
+++ Fitly/FitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitlyApp.swift
+//  Fitly
+//
+//  Created by Nandika P on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
